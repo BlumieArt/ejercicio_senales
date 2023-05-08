431323ff8b4e82e0ce008263d559f3b9a8128a0d
Belén Améndola <62146406+BlumieArt@users.noreply.github.com>
2023-05-08 14:05:58 -0300

    Creada la función generate_block
    PD: Tuve que acomodar los elementos de la escena Menu porque estaban todos en el origen. Los acomode para que se vean en mi pantalla lo más fiel al ejercicio posible, espero se vean en las otras bien.

--- scenes/Buttons/RedGenerator/RedGenerator.gd
+++ scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 
-
+var blockRed = preload("res://scenes/Blocks/Red/BlockRed.tscn")
 
 func _ready():
 	pass
@@ -12,4 +12,10 @@ func _process(delta):
 
 
 func _on_button_down():
+	generate_block()
 	pass 
+
+func generate_block():
+	var blockRedInstance = blockRed.instantiate()
+	add_child(blockRedInstance)
+	pass
--- scenes/Buttons/VioletGenerator/VioletGenerator.gd
+++ scenes/Buttons/VioletGenerator/VioletGenerator.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 
-
+var blockViolet = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
 
 func _ready():
 	pass 
@@ -12,4 +12,10 @@ func _process(delta):
 
 
 func _on_button_down():
+	generate_block()
 	pass 
+	
+func generate_block():
+	var blockVioletInstance = blockViolet.instantiate()
+	add_child(blockVioletInstance)
+	pass
