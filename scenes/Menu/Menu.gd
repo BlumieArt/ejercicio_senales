016b62554e85349e6b5fe049d5e7994b35c67240
Belén Améndola <62146406+BlumieArt@users.noreply.github.com>
2023-05-08 15:25:32 -0300

    Conectadas las escenas de generadores y el menu manualmente

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -8,6 +8,16 @@ func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	
+	#OPCION 1 
+	var redGen = get_node("RedGenerator")
+	redGen.button_down.connect(count_new_instance)
+	var violetGen = get_node("VioletGenerator")
+	violetGen.button_down.connect(count_new_instance)
+	
+	#OPCION 2 (las dos me funcionaron)
+	#$RedGenerator.button_down.connect(count_new_instance)
+	#$VioletGenerator.button_down.connect(count_new_instance)
 	pass
 
 func count_new_instance():
